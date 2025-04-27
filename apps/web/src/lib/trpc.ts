e14a1ce71a308c057ef4d767eaddbc90a5c7253d
Javier Segovia <jsegoviadev@gmail.com>
2025-04-27 17:32:39 -0400

    refactor: remove web folder

--- apps/web/src/lib/trpc.ts
+++
@@ -1,4 +0,0 @@
-import type { TRPCAppRouter } from '@gingga/api/src/trpc/routers/index'
-import { createTRPCContext } from '@trpc/tanstack-react-query'
-
-export const { TRPCProvider, useTRPC, useTRPCClient } = createTRPCContext<TRPCAppRouter>()
